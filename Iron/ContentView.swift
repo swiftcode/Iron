49dc79db1991c805deda5fd01c9b330e58ecba01
mpc <swiftcode@users.noreply.github.com>
2022-11-11 23:48:09 -0600

    Toolbar + SplitView
    Use a splitview
    Add a toolbar
    
    Changes to be committed:
    	modified:   Iron.xcodeproj/project.pbxproj
    	modified:   Iron/ContentView.swift
    	modified:   Iron/Iron.entitlements

--- Iron/ContentView.swift
+++ Iron/ContentView.swift
@@ -9,12 +9,104 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView {
-            HStack {
-                SideView()
-                FileView()
+         HSplitView {
+             NavigationView {
+                Section {
+                    VStack {
+                        Text("Sidebar")
+                        Text("Sidebar")
+                        Text("Sidebar")
+                        Text("Sidebar")
+                        Text("Sidebar")
+                    }
+                }
+                .frame(minWidth: 100, maxWidth: .infinity, minHeight: 100, maxHeight: .infinity, alignment: .topLeading)
+                .background(Color(red: 211/255, green: 208/255, blue: 208/255))
+
+                List() {
+                    Text("Content")
+                    Text("Content")
+                    Text("Content")
+                    Text("Content")
+                }
             }
-            .frame(minWidth: 300, idealWidth: 300, maxWidth: 300, maxHeight: .infinity)
+            .frame(minWidth: 100, maxWidth: .infinity, minHeight: 100, maxHeight: .infinity, alignment: .topLeading)
+            .toolbar(content: {
+
+                                ToolbarItem(placement: .automatic) {
+                                    Button(action: {
+
+                                    }) {
+                                        Image(systemName: "lessthan")
+                                            .imageScale(.large)
+                                    }
+                                }
+
+                                ToolbarItem(placement: .automatic) {
+                                    Button(action: {
+
+                                    }) {
+                                        Image(systemName: "greaterthan")
+                                            .imageScale(.large)
+                                    }
+                                    .padding(EdgeInsets(top: 0, leading: -15.0, bottom: 0, trailing: 0))
+                                }
+
+                                ToolbarItem(placement: .automatic) {
+                                    Button(action: {
+
+                                    }) {
+                                        Image(systemName: "list.bullet")
+                                            .imageScale(.large)
+                                    }
+                                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20))
+                                }
+
+                                ToolbarItem(placement: .automatic) {
+                                    Button(action: {
+
+                                    }) {
+                                        Image(systemName: "square.grid.3x1.below.line.grid.1x2")
+                                            .imageScale(.large)
+                                    }
+                                }
+
+                                ToolbarItem(placement: .automatic) {
+                                    Button(action: {
+
+                                    }) {
+                                        Image(systemName: "square.and.arrow.up")
+                                            .imageScale(.large)
+                                    }
+                                }
+
+                                ToolbarItem(placement: .automatic) {
+                                    Button(action: {
+
+                                    }) {
+                                        Image(systemName: "tag")
+                                            .imageScale(.large)
+                                    }
+                                }
+
+                                ToolbarItem(placement: .automatic) {
+                                    Button(action: {
+
+                                    }) {
+                                        Image(systemName: "ellipsis.circle")
+                                            .imageScale(.large)
+                                    }
+                                }
+
+                                ToolbarItem(placement: .automatic) {
+                                    Button(action: {
+
+                                    }) {
+                                        Image(systemName: "magnifyingglass")
+                                            .imageScale(.large)
+                                    }
+                                }
+                            })
         }
     }
 }
