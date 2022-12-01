7b14e20224ffcb783dd75a4d8e5e5466d71bbe3d
mpc <swiftcode@users.noreply.github.com>
2022-12-01 15:12:49 -0600

    Advanced Settings
    Create screen for the Advanced Settings options
    
    Changes to be committed:
    	modified:   Iron/Constants/Constants.swift
    	modified:   Iron/Preferences/AdvancedSettingsView.swift

--- Iron/Constants/Constants.swift
+++ Iron/Constants/Constants.swift
@@ -9,11 +9,22 @@ import Foundation
 
 struct Constants {
     struct Settings {
+        //General Settings
         static let hardDisks = "HARDDISKS"
         static let externalDisks = "EXTERNALDISKS"
         static let devices = "DEVICES"
         static let connectedServers = "CONNECTEDSERVERS"
         static let newWindow = "NEWWINDOW"
         static let openInTabs = "OPENINTABS"
+
+        //Advanced Settings
+        static let showFileExtensions = "SHOWFILEEXTENSIONS"
+        static let showWarningBeforeChangingExtension = "WARNINGBEFORECHANGINGEXTENSION"
+        static let showWarningBeforeRemovingFromiCloud = "WARNINGBEFOREREMOVINGFROMICLOUD"
+        static let showWarningBeforeEmptyingTrash = "WARNINGBEFOREEMPTYINGTRASH"
+        static let removeItemsFromTrash = "REMOVEITEMSFROMTRASH"
+        static let keepFoldersOnTopWhenSortingByName = "KEEPFOLDERSONTOPWHENSORTING"
+        static let keepFoldersOnTopOnDesktop = "KEEPFOLDERSONTOPONDESKTOP"
+
     }
 }
