112a617d09390ae4cb322b7cc073f913da7046e4
Michael Campbell <mpc@Michaels-Mac-mini.local>
2022-12-06 01:50:26 -0600

    Settings Layout
    Fix layout issue for individual settings by removing frame from overall settings screen (which limited the height dimension).
    Changes to be committed:
    	modified:   Iron/Preferences/PreferencesToolbar.swift
    	modified:   Iron/Preferences/SidebarSettingsView.swift

--- Iron/Preferences/SidebarSettingsView.swift
+++ Iron/Preferences/SidebarSettingsView.swift
@@ -22,60 +22,78 @@ struct SidebarSettingsView: View {
 
 
     var body: some View {
-        Form {
-            Text("Show these items on the desktop")
-
-            Group {
-                HStack {
-                    Toggle("", isOn: $showRecents)
-                    ImageLabel(img: "clock", label: "Recents")
-                }
-
-                HStack {
-                    Toggle("", isOn: $showAirdrop)
-                    ImageLabel(img: "dot.radiowaves.left.and.right", label: "Airdrop")
-                }
-
-                HStack {
-                    Toggle("", isOn: $showApplications)
-                    ImageLabel(img: "app", label: "Applications")
-                }
-
-                HStack {
-                    Toggle("", isOn: $showDesktop)
-                    ImageLabel(img: "menubar.dock.rectangle", label: "Desktop")
-                }
-
-                HStack {
-                    Toggle("", isOn: $showDocuments)
-                    ImageLabel(img: "doc", label: "Documents")
-                }
-            }
-
-            Group {
-                HStack {
-                    Toggle("", isOn: $showDownloads)
-                    ImageLabel(img: "arrow.down.circle", label: "Downloads")
+        VStack {
+            Form {
+                Text("Show these items on the desktop")
+                
+                Group {
+                    HStack {
+                        Toggle("", isOn: $showRecents)
+                        ImageLabel(img: "clock", label: "Recents")
+                    }
+                    
+                    HStack {
+                        Toggle("", isOn: $showAirdrop)
+                        ImageLabel(img: "dot.radiowaves.left.and.right", label: "Airdrop")
+                    }
+                    
+                    HStack {
+                        Toggle("", isOn: $showApplications)
+                        ImageLabel(img: "app", label: "Applications")
+                    }
+                    
+                    HStack {
+                        Toggle("", isOn: $showDesktop)
+                        ImageLabel(img: "menubar.dock.rectangle", label: "Desktop")
+                    }
+                    
+                    HStack {
+                        Toggle("", isOn: $showDocuments)
+                        ImageLabel(img: "doc", label: "Documents")
+                    }
                 }
                 
-                HStack {
-                    Toggle("", isOn: $showMovies)
-                    ImageLabel(img: "film", label: "Movies")
+                Group {
+                    HStack {
+                        Toggle("", isOn: $showDownloads)
+                        ImageLabel(img: "arrow.down.circle", label: "Downloads")
+                    }
+                    
+                    HStack {
+                        Toggle("", isOn: $showMovies)
+                        ImageLabel(img: "film", label: "Movies")
+                    }
+                    
+                    HStack {
+                        Toggle("", isOn: $showMusic)
+                        ImageLabel(img: "music.quarternote.3", label: "Music")
+                    }
+                    
+                    HStack {
+                        Toggle("", isOn: $showPictures)
+                        ImageLabel(img: "camera", label: "Pictures")
+                    }
+                    
+                    HStack {
+                        Toggle("", isOn: $showMpc)
+                        ImageLabel(img: "house", label: "mpc")
+                    }
                 }
                 
-                HStack {
-                    Toggle("", isOn: $showMusic)
-                    ImageLabel(img: "music.quarternote.3", label: "Music")
-                }
-                
-                HStack {
-                    Toggle("", isOn: $showPictures)
-                    ImageLabel(img: "camera", label: "Pictures")
-                }
-                
-                HStack {
-                    Toggle("", isOn: $showMpc)
-                    ImageLabel(img: "house", label: "mpc")
+                Group {
+                    Text("iCloud")
+                        .padding(EdgeInsets(top: 10, leading: 0, bottom: 5, trailing: 0))
+                    
+                    HStack {
+                        Toggle("", isOn: $showPictures)
+                        ImageLabel(img: "cloud", label: "iCloud Drive")
+                    }
+                    
+                    HStack {
+                        Toggle("", isOn: $showMpc)
+                        ImageLabel(img: "folder.badge.plus", label: "Shared")
+                    }
+                    
                 }
             }
         }
